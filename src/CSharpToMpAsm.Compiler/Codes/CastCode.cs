4cb33592861bcc81d9aa7a2db8b8b38c6f479edd
oshkoderov.oleg <oshkoderov.oleg@gmail.com>
2015-03-15 23:02:08 +0200

    Case from smaller to bigger types support added.

--- src/CSharpToMpAsm.Compiler/Codes/CastCode.cs
+++ src/CSharpToMpAsm.Compiler/Codes/CastCode.cs
@@ -6,12 +6,13 @@ namespace CSharpToMpAsm.Compiler.Codes
     internal class CastCode : ICode
     {
         private readonly TypeDefinition _type;
-        private readonly ICode _code;
+        private ResultLocation _location;
+        public ICode Code { get; private set; }
 
         public CastCode(TypeDefinition type, ICode code)
         {
             _type = type;
-            _code = code;
+            Code = code;
         }
 
         public TypeDefinition ResultType
@@ -21,35 +22,56 @@ namespace CSharpToMpAsm.Compiler.Codes
 
         public ResultLocation Location
         {
-            get {
-                if (ResultType == _code.ResultType)
-                {
-                    return _code.Location;
-                }
-                if (ResultType == TypeDefinitions.Byte)
-                {
-                    return ResultLocation.WorkRegister;
-                }
-                if (ResultType == TypeDefinitions.SByte)
-                {
-                    return ResultLocation.WorkRegister;
-                }
-                if (ResultType == TypeDefinitions.Int32)
-                {
-                    throw new NotImplementedException();
-                }
-                throw new NotSupportedException();
-            }
+            get { return _location; }
         }
 
         public void WriteMpAsm(IMpAsmWriter writer, IMemoryManager memManager)
         {
-            _code.WriteMpAsm(writer, memManager);
-            if (!_code.Location.IsWorkRegister)
+            Code.WriteMpAsm(writer, memManager);
+
+            if (ResultType == Code.ResultType)
+            {
+                _location =  Code.Location;
+                return;
+            }
+            if (ResultType == TypeDefinitions.Byte || ResultType == TypeDefinitions.SByte)
+            {
+                _location = ResultLocation.WorkRegister;
+
+                if (!Code.Location.IsWorkRegister)
+                {
+                    writer.Comment(string.Format("; Cast to {0} type.", ResultType));
+                    writer.MoveFileToW(Code.Location);
+                }
+                return;
+            }
+            
+            if (ResultType == TypeDefinitions.Int32)
             {
                 writer.Comment(string.Format("; Cast to {0} type.", ResultType));
-                writer.MoveFileToW(_code.Location);
+
+                _location = memManager.Alloc(ResultType.Size);
+
+                if (Code.ResultType.IsSigned() && Code.ResultType.Size < ResultType.Size)
+                {
+                    throw new NotImplementedException();
+                }
+
+                for (int i = 0; i < ResultType.Size; i++)
+                {
+                    if (Code.ResultType.Size > i)
+                    {
+                        writer.MoveFileToW(Code.Location + i);
+                        writer.MoveWToFile(_location + i);
+                    }
+                    else
+                    {
+                        writer.ClearFile(_location + i);
+                    }
+                }
+                return;
             }
+            throw new NotSupportedException();
         }
     }
 }
