309c86b86df5c8cff4bb90f505369f903a373461
oshkoderov.oleg <oshkoderov.oleg@gmail.com>
2015-03-25 19:08:54 +0200

    #12. InlineMethodVisitor Memory optimization - return variable eliminating in case of Assign.

--- src/CSharpToMpAsm.Compiler/Codes/InlineMethodVisitor.cs
+++ src/CSharpToMpAsm.Compiler/Codes/InlineMethodVisitor.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CSharpToMpAsm.Compiler.Codes
 {
@@ -15,40 +14,29 @@ namespace CSharpToMpAsm.Compiler.Codes
 
         protected override ICode Optimize(Assign assign)
         {
-            ICode value;
-            var code = TryOptimize(assign.Code, out value);
-            if (ReferenceEquals(code, assign.Code))
+            var call = assign.Code as Call;
+            if (call== null)
                 return base.Optimize(assign);
 
-            if (value == null)
-                throw new InvalidOperationException(
-                    string.Format("Can't assign void to a desstination {0} of type {1}.",
-                        assign.Destination.Name,
-                        assign.Destination.Type));
+            var destination = assign.Destination;
 
-            var newCode = new BlockCode(new[] { code, new Assign(assign.Destination, value), });
-            return Visit(newCode);
+            var code = InlineCall(call, destination);
+            return Visit(code);
         }
 
         protected override ICode Optimize(Call call)
         {
+            //ToDo: throw new InvalidOperationException("Can't inline properly. Context information is required.");
             if (call.Method.ShouldInline)
             {
-                ICode value;
-                var newCode = TryOptimize(call, out value);
-                if (value != null)
-                    throw new InvalidOperationException("Warning! Check parent code.");
-
+                var newCode = InlineCall(call, null);
                 return Visit(newCode);
             }
             return base.Optimize(call);
         }
 
-        private ICode TryOptimize(ICode code, out ICode methodResult)
+        private ICode InlineCall(Call call, IValueDestination result)
         {
-            methodResult = null;
-            var call = code as Call;
-            if (call == null || !call.Method.ShouldInline) return code;
             var method = call.Method;
 
             var destinations = new Dictionary<string, IValueDestination>();
@@ -67,20 +55,13 @@ namespace CSharpToMpAsm.Compiler.Codes
                         continue;
                     }
                 }
-                {
-                    var variable = new Variable(x.Name, x.Type, x.Location);
-                    destinations.Add(x.Name, variable);
-                    codes.Add(new Assign(variable, argument));
-                }
-            }
 
-            Variable result = null;
-            if (method.ReturnType != TypeDefinitions.Void)
-            {
-                result = new Variable(method.Name + "Result", method.ReturnType);
-                methodResult = new GetValue(result);
-            }
+                var variable = new Variable(x.Name, x.Type, x.Location);
+                destinations.Add(x.Name, variable);
+                codes.Add(new Assign(variable, argument));
 
+            }
+            
             var translated = new MethodBodyVisitor(destinations, result).Visit(method.Body);
 
             codes.Add(translated);
@@ -125,9 +106,9 @@ namespace CSharpToMpAsm.Compiler.Codes
         private class MethodBodyVisitor : CodeOptimisationVisitor
         {
             private readonly Dictionary<string, IValueDestination> _variables;
-            private readonly Variable _result;
+            private readonly IValueDestination _result;
 
-            public MethodBodyVisitor(Dictionary<string, IValueDestination> variables, Variable result)
+            public MethodBodyVisitor(Dictionary<string, IValueDestination> variables, IValueDestination result)
             {
                 _variables = variables;
                 _result = result;
